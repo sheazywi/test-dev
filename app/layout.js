7dfe3156c116fea453d46790d77fec67f25f7e33
sheazy_wi <sheazywi@gmail.com>
2024-05-09 11:19:08 -0400

    Ajout de...
     - Dossier components
     - Component nommé Footer qui sera le footer de la page
     - Component nommé Navbar donc qui sera le haut de la page, qui va nous  guidé vers d'autre pages.

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Footer from "@/components/Footer";
+import Navbar from "@/components/Navbar";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -11,7 +13,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <Navbar/>
       <body className={inter.className}>{children}</body>
+      <Footer/>
     </html>
   );
 }
