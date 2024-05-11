81b765dd71f9d3e3c74919732a64aa1a46be4b81
sheazy_wi <sheazywi@gmail.com>
2024-05-11 17:36:35 -0400

    added a text instead of image has a logo and changed the svg icon for a search one for a possible search bar

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -2,13 +2,14 @@ import React from 'react';
 import Image from 'next/image';
 import logo from '@/public/assets/images/logo.png';
 import { navLists } from '@/app/layout';
-import { BiSolidDashboard } from "react-icons/bi";
+import { IoSearchSharp } from "react-icons/io5";
 
 const Navbar = () => {
   return (
     <header className='w-full py-5 sm:px-10 px-5 flex justify-between items-center bg-black'>
       <nav className='flex w-full screen-max-width'>
-        {<Image className='' src={logo} alt="Logo" width={48} height={48}/>}
+        {/*<Image src={logo} alt="Logo" width={48} height={48}/>*/}
+        {<p className='text-3xl text-white'>starbounded</p>}
 
         <div className='flex flex-1 justify-center max-sm:hidden '>
           {navLists.map((nav) => (
@@ -18,11 +19,9 @@ const Navbar = () => {
           ))}
         </div>
 
-        {/*
-        <div className='flex items-baseline gap-7 max-sm:justify-end max-sm:flex-1'>
-          <BiSolidDashboard className='cursor-pointer text-gray-500 hover:text-white transition-all' size={35}/>
-        </div>
-        */}
+        {/*<div className='flex items-baseline gap-7 max-sm:justify-end max-sm:flex-1 justify-center'>
+          <IoSearchSharp className='cursor-pointer text-gray-500 hover:text-white transition-all place-content-center' size={50}/>
+        </div>*/}
       </nav>
     </header>
   )
