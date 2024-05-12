453c19269bceb9a8cacb0bb4af34ca31dca3d77f
sheazy_wi <sheazywi@gmail.com>
2024-05-12 06:13:13 -0400

    fixed some issues

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -3,7 +3,7 @@ import { styles } from '@/app/styles';
 
 const Hero = () => {
   return (
-    <section className="relative w-full h-[50vh] mx-auto bg-black">
+    <section className="relative w-full h-full mx-auto bg-black">
       <div className={`${styles.paddingX} absolute inset-0 top-[120px] max-w-7xl mx-auto flex flex-row items-start gap-5`}>
         <div className='flex flex-col justify-center items-center mt-5'>
           <div className='w-5 h-5 rounded-full bg-[#915eff]' />
