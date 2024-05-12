8bf78e798075d1fad57648b428d9575bc4e45177
sheazy_wi <sheazywi@gmail.com>
2024-05-12 01:18:28 -0400

    stupid stuff to fix in team, will do it later

--- components/Team.jsx
+++ components/Team.jsx
@@ -8,14 +8,14 @@ import { Separator } from "@/components/ui/separator"
 const Team = () => {
   return (
     <section className="relative w-full mx-auto bg-black h-screen">
-        <div className={`${styles.paddingX} absolute inset-0 top-[120px] max-w-7xl mx-auto flex flex-row items-start gap-5`}>
+        <div className={`${styles.paddingX} absolute inset-0 top-[120px] mb-20 max-w-7xl mx-auto flex flex-row items-start gap-5`}>
           {team.map(({ id, name, image, description, position }) => (
             <div key={id} className='px-10 w-full h-full bg-clip-border justify-around rounded-lg text-lg bg-black border-4 border-violet-600'>
-              <div className='text-gray-500 transition-all'>
+              <div className='text-gray-500'>
                 <div className='mt-12 flex items-center justify-center'>
                   <Image className='rounded-full' src={`/assets/images/people/${image}`} alt={name} width={256} height={256} />
                 </div>
-                <div className='flex flex-col items-center mt-12 text-2xl'>
+                <div className='flex flex-col items-center mt-12 text-2xl md:text-normal'>
                   <p>
                     <b>{name}</b>
                   </p>
@@ -24,7 +24,7 @@ const Team = () => {
                   </p>
                 </div>
                 <div>
-                  <p className='text-center mt-8 text-normal'>
+                  <p className='text-center mt-8 lg:text-normal dm:text-xs'>
                     {description}
                   </p>
                 </div>
