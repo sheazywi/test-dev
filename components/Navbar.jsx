3856f821c010fc136927f1362ece84e61f6ac0f4
sheazy_wi <sheazywi@gmail.com>
2024-05-12 06:04:15 -0400

    made some changes for the display size only for the navbar

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -3,29 +3,29 @@ import Image from 'next/image';
 import logo from '@/public/assets/images/logo.png';
 import { navLists } from '@/app/layout';
 import { IoSearchSharp } from "react-icons/io5";
+import { RxHamburgerMenu } from "react-icons/rx";
 import Link from 'next/link';
 
 const Navbar = () => {
   return (
-    <header className='w-full py-5 sm:px-10 px-5 flex justify-between items-center bg-black'>
+    <header className='w-full py-5 px-5 flex justify-between items-center bg-black'>
       <nav className='flex w-full navbar max-w-7xl gap-5 mx-auto justify-between'>
         {/*<Image src={logo} alt="Logo" width={48} height={48}/>*/}
-        {<p className='text-3xl text-white'>starbounded</p>}
+        {<p className='md:text-3xl max-md:text-2xl text-white'>starbounded</p>}
 
-        <div className='flex max-md:hidden'>
-          {navLists.map(({ id, link, name }) => (
-              <div key={id} className='px-10 text-gray-500 transition-all place-content-center text-lg'>
-                <div>
-                  <Link href={link}>
-                    <p className='cursor-pointer hover:text-white'>
-                      {name}
-                    </p>
-                  </Link>
+        <div className='flex'>
+            {navLists.map(({ id, link, name }) => (
+                <div key={id} className='max-md:px-6 md:px-10 md:text-lg max-md:text-normal text-gray-500 transition-all place-content-center'>
+                  <div>
+                    <Link href={link}>
+                      <p className='cursor-pointer hover:text-white'>
+                        {name}
+                      </p>
+                    </Link>
+                  </div>
                 </div>
-              </div>
-            ))}
+              ))}
         </div>
-
         {/*<div className='flex items-baseline gap-7 max-sm:justify-end max-sm:flex-1 justify-center'>
           <IoSearchSharp className='cursor-pointer text-gray-500 hover:text-white transition-all place-content-center' size={50}/>
         </div>*/}
