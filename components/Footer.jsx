08206b15d553ebb04369242de8df8a170de736fa
sheazy_wi <sheazywi@gmail.com>
2024-05-14 18:53:51 -0400

    Update Footer.jsx

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,14 +1,16 @@
-import React from 'react'
-import { Separator } from "@/components/ui/separator"
+import React from 'react';
+import { Separator } from "@/components/ui/separator";
+import Link from 'next/link';
 
 const Footer = () => {
   return (
     <footer className="w-full py-5 sm:px-10 px-5 flex justify-between items-center bg-black footer text-white">
       <div className='w-full'>
-        <div className="space-y-1">
-          <p className='flex flex-1 justify-center'>
-            <b>starbounded</b>
-          </p>
+      <div className="space-y-1">
+          <div className='flex flex-1 justify-center gap-8 flex-row text-normal'>
+            <Link className='cursor-pointer text-white hover:underline' href='https://github.com/starbounded-dev'>github</Link>
+            <Link className='cursor-pointer text-white hover:underline' href='https://discord.gg/Nsh6P6X2KK'>discord</Link>
+          </div>
         </div>
         <Separator className="my-5" />
         <div className="flex flex-1 justify-center space-x-4 text-normal">
